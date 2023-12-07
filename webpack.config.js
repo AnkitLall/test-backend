596bebd9e882cd646c393f5aaab47ba0a958f6c4
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2023-12-07 00:30:40 -0500

    t

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  target: 'node',
+  entry: './server.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
+
+export default config;
